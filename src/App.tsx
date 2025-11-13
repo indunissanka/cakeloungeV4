c45aa01a62df53939043ce3638b66ce81684fdd1
indunissanka <68851144+indunissanka@users.noreply.github.com>
2025-11-13 10:44:54 +0800

    remove gallery

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import CakeCustomizer from './components/CakeCustomizer';
 import { Contact } from './components/Contact';
-import Gallery from './components/Gallery';
 import PrivacyPolicy from './components/PrivacyPolicy';
 
 const MainPage: React.FC = () => (
   <>
     <div className="bg-pink-100 text-center py-2 text-gray-700 font-bold text-xl">
       The Cake Lounge
-      <div className="mt-2">
-        <Link to="/gallery" className="text-blue-600 hover:text-blue-800 underline">
-          View Gallery
-        </Link>
-      </div>
     </div>
     <CakeCustomizer />
     <Contact />
@@ -29,7 +23,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="/gallery" element={<Gallery />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
